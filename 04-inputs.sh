8a97911f541b41eda1e5d00d4102669a5ebbdce7
manasa <manasa.busireddy@gmail.com>
2023-02-22 18:51:05 -0500

    shell scripting topics

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,4 +1,13 @@
-echo $1
-echo $2
-  
 
+
+# 0-n , $*/$@, $#
+
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+
+echo Number of Arguments = $# # Number of Arguments
