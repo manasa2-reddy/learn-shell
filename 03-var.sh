bf22cb3ee216b8874313d85d0cb31845dd2d68fb
manasa <manasa.busireddy@gmail.com>
2023-02-22 18:47:39 -0500

    shell scripting topics

---
+++ 03-var.sh
@@ -0,0 +1,16 @@
+es (10 sloc)  272 Bytes
+
+DATE=2023-02-17
+echo "Hello, Good Morning, Today Date is ${DATE}"
+
+# Command Substitution
+DATE=$(date)
+echo "Hello, Good Morning, Today Date is ${DATE}"
+
+# Arithmetic Substitution
+ADD=$((2+3))
+echo "Addition of 2+3 = ${ADD}"
+
+
+## Accessing Variable from Shell
+echo x = $x
